422f2d9db14e71d992507423517c213f2c8a808f
Ciaran Treanor <ciaran.treanor@gmail.com>
2021-12-02 09:02:12 +0000

    Day03 Skeleton

---
+++ src/main/kotlin/Day03.kt
@@ -0,0 +1,9 @@
+/**
+ * Day 3: [TODO](https://adventofcode.com/2021/day/3)
+ */
+class Day03(private val input: List<String>) {
+
+    fun solvePart1() = 0
+
+    fun solvePart2() = 0
+}
---
+++ src/test/kotlin/Day03Test.kt
@@ -0,0 +1,41 @@
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+@DisplayName("Day 3")
+class Day03Test {
+    val exampleInputs = listOf(1, 2, 3)
+
+    @Nested
+    @DisplayName("Part 1")
+    inner class Part1 {
+
+        @Test
+        fun `Matches example`() {
+//            assertEquals(150, Day02(exampleInputs).solvePart1())
+        }
+
+        @Test
+        fun `Actual answer`() {
+//            val inputs = Resources.resourceAsList("day02.txt")
+//            assertEquals(2120749, Day02(inputs).solvePart1())
+        }
+    }
+
+    @Nested
+    @DisplayName("Part 2")
+    inner class Part2 {
+
+        @Test
+        fun `Matches example`() {
+//            assertEquals(900, Day02(exampleInputs).solvePart2())
+        }
+
+        @Test
+        fun `Actual answer`() {
+//            val inputs = Resources.resourceAsList("day02.txt")
+//            assertEquals(2138382217, Day02(inputs).solvePart2())
+        }
+    }
+}
